a505e8b0dfc7b98b1f5b6dbb363dd17b4e38fb52
KeeranNaidu12 <keerannaidu08@gmail.com>
2025-09-17 00:06:24 -0700

    Fixing NavBar styling (WIP)

--- esports-event-and-tournament-system/src/components/NavBar.tsx
+++ esports-event-and-tournament-system/src/components/NavBar.tsx
@@ -3,8 +3,10 @@ import Link from "next/link";
 
 function NavBar() {
   return (
-    <nav className="w-full top-0 bg-blue-600">
-      <div className="p-5 text-white font-weight space-x-5 text-lg font-medium">
+    <nav className="fixed top-0 left-0 right-0 bg-blue-600 z-50 p-5">
+      <div className="text-white font-weight space-x-5 text-lg font-medium">
+        <div></div>
+
         <Link href="/">Home</Link>
         <Link href="/about">About</Link>
         <Link href="/stuff">Events</Link>
