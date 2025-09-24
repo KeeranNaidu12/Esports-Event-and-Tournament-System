e2c7d5a7fecf520b77a929966b0e5bf851c45a72
KeeranNaidu12 <keerannaidu08@gmail.com>
2025-09-23 23:29:37 -0700

    Improving Executive Cards and added props

--- esports-event-and-tournament-system/src/app/about/page.tsx
+++ esports-event-and-tournament-system/src/app/about/page.tsx
@@ -17,23 +17,41 @@ export default function About() {
           Executives
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center">
-          <Exec />
-          <Exec />
-          <Exec />
-          <Exec />
-          <Exec />
-          <Exec />
-          <Exec />
+          <Exec name="Jaiden" role="President" pronouns="He/Him" />
+          <Exec name="Mark" role="VP Internal" pronouns="He/Him" />
+          <Exec name="Yug" role="VP Events" pronouns="He/Him" />
+          <Exec name="Nathan" role="VP Finance" pronouns="He/Him" />
+          <Exec name="Ali" role="Esports Director" pronouns="He/Him" />
+          <Exec name="Lexi" role="Graphics & Socials" pronouns="She/Her" />
+          <Exec name="Pho" role="Event Coordinator" pronouns="They/Them" />
         </div>
         {/* Game Coordinators */}
         <h2 className="font-bold text-5xl text-center text-white m-10 justify-items-center">
           Game Coordinators
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 justify-items-center">
-          <Exec />
-          <Exec />
-          <Exec />
-          <Exec />
+          <Exec name="Adrian" role="Valorant Director" pronouns="He/Him" />
+          <Exec
+            name="Jacky"
+            role="League of Legends Director"
+            pronouns="He/Him"
+          />
+          <Exec
+            name="Jason"
+            role="League of Legends Director"
+            pronouns="He/Him"
+          />
+          <Exec
+            name="Fissure"
+            role="Rocket League Director"
+            pronouns="He/Him"
+          />
+          <Exec
+            name="Jacob"
+            role="Super Smash Bros Director"
+            pronouns="He/Him"
+          />
+          <Exec name="Yug" role="Strategy Games Director" pronouns="He/Him" />
         </div>
       </main>
     </div>
--- esports-event-and-tournament-system/src/components/Exec.tsx
+++ esports-event-and-tournament-system/src/components/Exec.tsx
@@ -1,20 +1,27 @@
 import React from "react";
 
-function Exec() {
+interface ExecProps {
+  name: string;
+  role: string;
+  pronouns: string;
+}
+
+function Exec({ name, role, pronouns }: ExecProps) {
   return (
-    <div className="bg-indigo-900 rounded-2xl font-bold w-80 h-120 m-5 ml-20 overflow-hidden">
+    <div className="bg-indigo-900 rounded-2xl font-bold w-80  m-5 ml-20">
       <div className="flex justify-center p-5 m-5">
         <img
           src="/Venom.png"
           className="w-[240px] h-[240px] object-cover rounded-full"
         ></img>
       </div>
-      <h1 className="text-white text-4xl text-center">Venom</h1>
+      <h1 className="text-white text-4xl text-center">{name}</h1>
+      <div className="text-center bg-indigo-400 ml-20 mr-20 mt-3 rounded-2xl white">
+        <h2 className="text-white text-2xl text-center">{pronouns}</h2>
+      </div>
       <br />
-      <div className="grid grid-cols-3 text-center bg-indigo-400 ml-5 mr-5 rounded-2xl pl-3 pr-3">
-        <h2 className="text-white text-2xl">President </h2>
-        <h2 className="text-white text-2xl ml-5">•</h2>
-        <h2 className="text-white text-2xl ml-[-20px]">He/Him</h2>
+      <div className="text-center text-3xl mb-10 ml-5 mr-5 rounded-2xl white">
+        <h2 className="text-white text-2xl">{role} </h2>
       </div>
     </div>
   );
