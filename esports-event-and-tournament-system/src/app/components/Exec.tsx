b0f5d5c6f25b4ea0b8cd741cf1a87c97d1cfbf86
KeeranNaidu12 <keerannaidu08@gmail.com>
2025-09-24 13:26:55 -0700

    Recovering files

---
+++ esports-event-and-tournament-system/src/app/components/Exec.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+interface ExecProps {
+  name: string;
+  role: string;
+  pronouns: string;
+}
+
+function Exec({ name, role, pronouns }: ExecProps) {
+  return (
+    <div className="bg-indigo-900 rounded-2xl font-bold w-100 m-5 ml-20 mr-20">
+      <div className="flex justify-center p-5 m-5 mt-15">
+        <img
+          src="/Venom.png"
+          className="w-[240px] h-[240px] object-cover rounded-full"
+        ></img>
+      </div>
+      <h1 className="text-white text-4xl text-center">{name}</h1>
+      <div className="text-center bg-indigo-400 ml-20 mr-20 mt-3 rounded-2xl white">
+        <h2 className="text-white text-2xl text-center">{pronouns}</h2>
+      </div>
+      <br />
+      <div className="text-center text-3xl mb-10 ml-5 mr-5 rounded-2xl white">
+        <h2 className="text-white text-2xl">{role} </h2>
+      </div>
+    </div>
+  );
+}
+
+export default Exec;
---
+++ esports-event-and-tournament-system/src/app/login/page.tsx
@@ -0,0 +1,67 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+import Image from "next/image";
+import { login } from "./actions";
+
+export default function Login() {
+  return (
+    <div
+      className="min-h-screen bg-cover bg-center bg-no-repeat flex items-center justify-center"
+      style={{ backgroundImage: "url('/ubco.png')" }}
+    >
+      <NavBar />
+      <main>
+        <div className="mt-10 bg-indigo-900 p-5 rounded-3xl">
+          <div className="bg-indigo-600 pl-5 pr-5 rounded-t-lg">
+            <h1 className="text-white font-bold text-center text-2xl mb-5">
+              Are you a member of OKEA?
+              <br />
+              Log into your account to access exclusive features!
+            </h1>
+          </div>
+          <div className="flex flex-col items-center space-x-5">
+            <Image
+              className="flex justify-center mb-5 border-2 border-blue-500 w-30 h-30 rounded"
+              src="/OKEA_Logo_Shine.png.gif"
+              alt="UBCO_gif"
+              width={150}
+              height={100}
+              unoptimized
+            />
+            <form>
+              <div className="bg-indigo-600 p-5 rounded-3xl">
+                <div className="m-1">
+                  <label className="text-white font-semibold">Email: </label>
+                  <br />
+                  <input
+                    className="bg-gray-300 font-semibold rounded-2xl text-blue-600 pl-2"
+                    type="email"
+                    name="email"
+                    required
+                  />
+                </div>
+                <br />
+                <div className="m-1">
+                  <label className="text-white font-semibold">Password: </label>
+                  <br />
+                  <input
+                    className="bg-gray-300 font-semibold text-blue-600 rounded-2xl pl-2"
+                    type="password"
+                    name="password"
+                    required
+                  />
+                </div>
+                <button
+                  formAction={login}
+                  className="flex justify-center bg-indigo-500 pl-5 pr-5 mt-5 rounded-2xl text-white font-bold hover:bg-indigo-300"
+                >
+                  Log In
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
